ee0ef6a29b09f47937198fa47a7bb97f68f52f24
Loïc Butor <butorloic90@gmail.com>
2024-11-07 13:09:05 +0100

    Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    'nuxt-lodash',
+    '@formkit/nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt-mongoose'
+  ]
 })
