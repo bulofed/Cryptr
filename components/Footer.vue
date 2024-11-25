ff334344b5585869c5a62301c38dc34b46a4b646
Loïc Butor <loic.butor.game@gmail.com>
2024-11-25 10:36:23 +0100

    feat(style): Refactorisation du CSS
    - Afin de ne pas réécrire plusieurs fois les mêmes classes, on utilise [&>*] sur le parent.
    - Afin de pouvoir modifier le stroke et fill d'un SVG, on fait un component de celui-ci directement au lieu d'une balise img qui le référence.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="bg-neutral-800 w-screen flex flex-col px-16">
-    <div class="flex items-center justify-between py-8 border-b-2 border-red-500">
-      <p class="text-3xl font-semibold text-slate-50">Rejoignez nous !</p>
-      <div class="flex space-x-6">
-        <img src="~/public/github.svg" alt="Logo_github" class="size-8"/> 
-        <img src="~/public/mail.svg" alt="Logo_mail" class="size-8"> 
+    <div class="flex items-center justify-between py-8 border-b-2 border-purple-500">
+      <p class="text-3xl font-semibold">Rejoignez nous !</p>
+      <div class="flex space-x-6 [&>*]:size-8">
+        <IconGithub />
+        <IconMail />
       </div>
     </div>
-    <div class="flex justify-between py-4 text-xs text-slate-50">
+    <div class="flex justify-between py-4 text-xs">
       <p>@ 2024 Cryptr</p>
-      <ol class="flex">
-        <li class="border-e-2 border-red-500 pr-2 hover:underline">
-          Conditions d'utilisation
+      <ol class="flex [&>:not(:last-child)]:border-e-2 [&>:not(:last-child)]:border-purple-500 hover:[&>*]:underline space-x-2 [&>:not(:last-child)]:pr-2">
+        <li>
+          <NuxtLink to="/">Conditions d'utilisation</NuxtLink>
         </li>
-        <li class="border-e-2 border-red-500 px-2 hover:underline">
-          Politique de confidentialité
+        <li>
+          <NuxtLink to="/">Politique de confidentialité</NuxtLink>
         </li>
-        <li class="border-e-2 border-red-500 px-2 hover:underline">
-          Gestion des cookies
+        <li>
+          <NuxtLink to="/">Gestion des cookies</NuxtLink>
         </li>
-        <li class="pl-2 hover:underline">
-          Mentions légales
+        <li>
+          <NuxtLink to="/">Mentions légales</NuxtLink>
         </li>
       </ol>
     </div>
