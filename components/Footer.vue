412499c2157fa54d5a40bdd4591e7812f40ed800
Pierru Mathéo <matheopierru62176@gmail.com>
2024-11-28 13:40:47 +0100

    Fix ( index & footer ) :
    Footer : Correction des couleurs pour les logos ( Github , Mail )
    
    Index : Ajout des différentes information du projet

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="bg-neutral-800 w-screen flex flex-col px-16">
+  <div class="bg-neutral-800 w-min-screen flex flex-col px-16">
     <div
       class="flex items-center justify-between py-8 border-b-2 border-purple-500"
     >
-      <p class="text-3xl font-semibold">
+      <p class="text-3xl font-semibold  text-white">
         Rejoignez nous !
       </p>
       <div class="flex space-x-6 [&>*]:size-8">
-        <IconGithub />
-        <IconMail />
+        <IconGithub class="fill-white"/>
+        <IconMail class="fill-white" />
       </div>
     </div>
-    <div class="flex justify-between py-4 text-xs">
+    <div class="flex justify-between py-4 text-xs  text-white">
       <p>@ 2024 Cryptr</p>
       <ol
         class="flex [&>:not(:last-child)]:border-e-2 [&>:not(:last-child)]:border-purple-500 hover:[&>*]:underline space-x-2 [&>:not(:last-child)]:pr-2"
