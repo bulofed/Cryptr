8273cea29311430bc238bae6e6037c39f65fcd7a
Lecoustre Malcom <malcom.lecoustre@gmail.com>
2024-11-28 16:51:04 +0100

    feat(database): LoginPost
    Permet de vérifier que l'utilisateur est bien existant et renvoie un statut selon la réussite ou non

---
+++ server/api/auth/login.post.js
@@ -0,0 +1,47 @@
+import { defineEventHandler, readBody } from 'h3';
+import UtilisateurModel from '~/server/models/Utilisateur';
+import bcrypt from 'bcrypt';
+
+export default defineEventHandler(async (event) => {
+  const { identifier, motDePasse, isEmail } = await readBody(event);
+
+  try {
+    const user = await UtilisateurModel.findOne(
+      isEmail ? { email: identifier } : { username: identifier }
+    );
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(motDePasse, user.motDePasse);
+      if (isPasswordValid) {
+        return {
+          status: 200,
+          success: true,
+          message: 'Utilisateur trouvé et mot de passe valide',
+          user: {
+            email: user.email,
+            username: user.username,
+          },
+        };
+      } else {
+        return {
+          status: 401,
+          success: false,
+          message: 'Mot de passe incorrect',
+        };
+      }
+    } else {
+      return {
+        status: 422,
+        success: false,
+        message: 'Utilisateur non trouvé',
+      };
+    }
+  } catch (err) {
+    console.error('Erreur lors de la vérification de l\'utilisateur :', err);
+    return {
+      status: 500,
+      success: false,
+      message: 'Une erreur est survenue lors de la vérification de l\'utilisateur.',
+    };
+  }
+});
