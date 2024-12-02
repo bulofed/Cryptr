1693a8de5ba5748294e4a964f21ff4322bd4a4b0
Loïc Butor <butorloic90@gmail.com>
2024-12-02 08:58:10 +0100

    feat(page): Enigme
    Cette page possède un inventaire ainsi qu'un terminal fonctionnel.

---
+++ components/Inventory.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  count: {
+    type: Number,
+    default: 5
+  }
+})
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div class="flex gap-4 mt-4">
+      <div v-for="i in count" :key="i" class="size-32 bg-neutral-700 rounded-lg"></div>
+    </div>
+  </div>
+</template>
