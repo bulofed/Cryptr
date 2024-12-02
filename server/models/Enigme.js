d3473f8bd0c9b5f897e8d2774ca099f6078da93d
Lecoustre Malcom <malcom.lecoustre@gmail.com>
2024-12-02 14:33:31 +0100

    fix(database): Nom des champs
    Correction des noms des champs du modèle

--- server/models/Enigme.js
+++ server/models/Enigme.js
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose'
 
 const enigmeSchema = new mongoose.Schema({
-  titre: { type: String, required: true, trim: true },
-  description: { type: String },
-  niveauDifficulte: { type: Number },
-  tempsEstime: { type: Number },
-  indices: { type: [String] },
-  solution: { type: String },
-  categories: { type: [String] },
-  statistiques: {
-    tauxReussite: { type: Number },
-    nombreTentatives: { type: Number },
-    tempsMoyenResolution: { type: Number }
-  }
+  title : { type: String, required: true, trim: true },
+  description : { type: String },
+  difficultyLevel : { type: Number },
+  estimatedTime : { type: Number },
+  clues : { type: [String] },
+  solution : { type: String },
+  categories : { type: [String] },
+  statistics : {
+    successRate : { type: Number },
+    tries : { type: Number },
+    averageResolutionTime : { type: Number }
+  },
+  unlockedClues : { type: [String] },
+  imgPath : { type: String },
+  textInspect : { type: String },
+  unlockedEnigma : { type: [String] },
 })
 
 const EnigmeModel = mongoose.model('Enigme', enigmeSchema, 'Enigme')
