05ac56e7baed01669de153d906dee3894f852c26
NoName-coding <hugohallot@gmail.com>
2024-12-05 10:14:35 +0100

    feat(api): IdPatch
    Permet de capter les Patch pour la base de données

---
+++ server/api/utilisateurs/[id].patch.js
@@ -0,0 +1,38 @@
+import UtilisateurModel from "~/server/models/Utilisateur";
+
+export default defineEventHandler(async (event) => {
+  const { id } = event.context.params;
+  const body = await readBody(event);
+
+  try {
+    const user = await UtilisateurModel.findOneAndUpdate(
+      { username: id },
+      { $set: body },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return {
+        status: 404,
+        success: false,
+        message: `Aucun utilisateur trouvé avec le nom d'utilisateur : ${username}.`,
+      };
+    }
+
+    return {
+      status: 200,
+      success: true,
+      message: "Utilisateur mis à jour avec succès.",
+      data: user,
+    };
+  } catch (err) {
+    console.error("Erreur lors de la mise à jour de l'utilisateur :", err);
+    return {
+      status: 500,
+      success: false,
+      message:
+        "Une erreur est survenue lors de la mise à jour de l'utilisateur.",
+      error: err.message || "Erreur inconnue",
+    };
+  }
+});
