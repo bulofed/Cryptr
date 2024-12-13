d7c15604431fc77b73f871e1cfa96a9cb0b4d495
Loïc Butor <butorloic90@gmail.com>
2024-12-13 16:25:50 +0100

    feat(css): Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-neutral-800 w-min-screen flex flex-col px-16">
+  <div class="w-min-screen flex flex-col px-16">
     <div class="flex items-center justify-between py-8 border-b-2 border-purple-500">
-      <p class="text-3xl font-semibold  text-white">
+      <p class="text-3xl font-semibold">
         Rejoignez nous !
       </p>
       <div class="flex space-x-6 [&>*]:size-8">
@@ -9,7 +9,7 @@
         <IconMail class="fill-white" />
       </div>
     </div>
-    <div class="flex justify-between py-4 text-xs  text-white">
+    <div class="flex justify-between py-4 text-xs">
       <p>@ 2024 Cryptr</p>
       <ol
         class="flex [&>:not(:last-child)]:border-e-2 [&>:not(:last-child)]:border-purple-500 hover:[&>*]:underline space-x-2 [&>:not(:last-child)]:pr-2"
