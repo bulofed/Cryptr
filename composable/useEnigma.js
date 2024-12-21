c843dfbbcc220198f22ad4a1fd8cec28cd32a415
Lecoustre Malcom <malcom.lecoustre@gmail.com>
2024-12-21 16:03:23 +0100

    fix(composables): useEnigma.js
    Ajout du fetchUnlockedEnigmas afin de récupérer les énigmes débloqué du user

--- composable/useEnigma.js
+++ composable/useEnigma.js
@@ -1,4 +1,6 @@
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { fetchUser } from '~/composable/useUser';
 
 export const useEnigma = () => {
   const route = useRoute();
@@ -22,5 +24,45 @@ export const useEnigma = () => {
 
   return {
     fetchCurrentEnigma
+  };
+};
+
+export const useUnlockedEnigmas = () => {
+  const enigmes = ref([]);
+  const loading = ref(false);
+  const error = ref(null);
+
+  const fetchUnlockedEnigmas = async (user) => {
+    try {
+      loading.value = true;
+      const response = await $fetch('/api/enigmes');
+      
+      if (response.success) {
+        const allEnigmas = response.data;
+      
+        const userData = await fetchUser(user.value.username);
+        
+        if (userData && userData.unlockedEnigmas) {
+          const unlockedTitles = userData.unlockedEnigmas.map(unlocked => unlocked.title);
+          enigmes.value = allEnigmas.filter(enigma => unlockedTitles.includes(enigma.title));
+        } else {
+          enigmes.value = [];
+        }
+      } else {
+        error.value = 'Erreur de chargement';
+      }
+    } catch (error) {
+      console.error('Erreur:', error);
+      error.value = 'Erreur lors de la récupération des énigmes';
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    enigmes,
+    loading,
+    error,
+    fetchUnlockedEnigmas
   };
 };
