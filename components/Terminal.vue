081b0e7cdb09636a9eb282eff52514fc79b1d96d
NoName-coding <hugohallot@gmail.com>
2025-01-10 13:39:25 +0100

    Update Terminal.vue

--- components/Terminal.vue
+++ components/Terminal.vue
@@ -98,7 +98,7 @@ const commands = {
 
       if (arg === enigma.solution) { 
         clearInterval(timerInterval);
-        terminalLines.value.push(enigma.completionMessage);
+        typeText(enigma.completionMessage);
 
         if (user.value) {
           const userData = await fetchUser(user.value.username);
@@ -143,11 +143,13 @@ const commands = {
           }
         }
       } else {
-        terminalLines.value.push('Wrong. Try again!');
+        
         const userData = await fetchUser(user.value.username);
         const enigmaIndex = userData.unlockedEnigmas.findIndex(e => e.title === enigma.title);
+        
         if (userData.unlockedEnigmas[enigmaIndex].state !== 'solved') {
           userData.unlockedEnigmas[enigmaIndex].numberOfTry += 1;
+          typeText('Wrong. Try again! you tried ' + (userData.unlockedEnigmas[enigmaIndex].numberOfTry) + ' times so far');
           await $fetch(`/api/utilisateurs/${user.value.username}`, {
                   method: 'PATCH',
                   headers: { 'Content-Type': 'application/json' },
@@ -190,7 +192,7 @@ const commands = {
   '/story':{
     description: 'gives the synopsis of the story',
     action:() => {
-      terminalLines.value.push("narrateur : Cela faisait des mois que John Smith était dans sa quête pour retrouver le joyau perdu d'Athènes, une magnifique pierre précieuse documentée à multiples reprises à travers l'histoire, dont les mythes expliquent qu'elle disparaissait inexplicablement à la mort de son porteur, puis réapparaissait de manière tout aussi inexpliquée dans les mains de divers intellectuels à travers le monde. \nLa pierre a inspiré de multiples légendes depuis l'antiquité, mais John n'est pas du genre superstitieux, pour lui ce saphir n'était qu'un moyen de sécuriser un beau pactole et de gagner en célébrité, rien de plus. \nIl tremblait d'excitation, cela faisait des décennies que la pierre n'avait pas été vue et en retraçant la vie de ses anciens détenteurs, il trouvait constamment un point commun, une visite dans une certaine région de Grèce.\n Après quelques jours sur place, il avait enfin trouvé ce qu'il cherchait si avidement, une entrée vers des ruines souterraines, à peine visible, couverte de végétation et sur le flanc d'une montagne.\nLa descente fut longue et minutieuse, John déjouant des dizaines de pièges après avoir fait l'erreur d'assumer qu'aucun ne serait présent, une erreur qui lui coûta un doigt.\nMais il y était enfin arrivé, il pouvait le sentir, après cette longue descente il était enfin dans une salle qui n'était non pas un long couloir sinueux, mais une large antichambre avec 5 portes en pierre face à lui.\nBonne chance aventurier, c'est maintenant à vous de faire progresser notre cher John Smith dans sa quête de richesse et de gloire. ")
+      typeText("narrateur : Cela faisait des mois que John Smith était dans sa quête pour retrouver le joyau perdu d'Athènes, une magnifique pierre précieuse documentée à multiples reprises à travers l'histoire, dont les mythes expliquent qu'elle disparaissait inexplicablement à la mort de son porteur, puis réapparaissait de manière tout aussi inexpliquée dans les mains de divers intellectuels à travers le monde. \nLa pierre a inspiré de multiples légendes depuis l'antiquité, mais John n'est pas du genre superstitieux, pour lui ce saphir n'était qu'un moyen de sécuriser un beau pactole et de gagner en célébrité, rien de plus. \nIl tremblait d'excitation, cela faisait des décennies que la pierre n'avait pas été vue et en retraçant la vie de ses anciens détenteurs, il trouvait constamment un point commun, une visite dans une certaine région de Grèce.\n Après quelques jours sur place, il avait enfin trouvé ce qu'il cherchait si avidement, une entrée vers des ruines souterraines, à peine visible, couverte de végétation et sur le flanc d'une montagne.\nLa descente fut longue et minutieuse, John déjouant des dizaines de pièges après avoir fait l'erreur d'assumer qu'aucun ne serait présent, une erreur qui lui coûta un doigt.\nMais il y était enfin arrivé, il pouvait le sentir, après cette longue descente il était enfin dans une salle qui n'était non pas un long couloir sinueux, mais une large antichambre avec 5 portes en pierre face à lui.\nBonne chance aventurier, c'est maintenant à vous de faire progresser notre cher John Smith dans sa quête de richesse et de gloire. ")
     }
   }
 }
